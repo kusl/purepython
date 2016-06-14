f081a977d683c1c8131f4fa5fbe6b98a1e1ec1d8
kushal <kushaldeveloper@gmail.com>
2016-06-13 21:56:07 -0400

    fail

--- home.py
+++ home.py
@@ -7,8 +7,8 @@ print(fibonacci(9))
 my_matrix = numpy.matrix('1 2; 3 4')
 print(my_matrix.getI() * my_matrix)
 my_list = [12, 5, 13, 8, 9, 65]
-if bubble(my_list) == [5, 8, 9, 12, 13, 65]:
-    print("success")
-else:
-    print("failure")
+my_new_list = bubble(my_list)
+for index in range(1, 6):
+    if my_new_list[index] < my_new_list[index - 1]:
+        print("failure")
 print("bye")
