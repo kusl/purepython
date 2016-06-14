a12b0ac4b17b39ecee96b81eabc0bdb9d00c2966
kushal <kushaldeveloper@gmail.com>
2016-06-13 21:49:53 -0400

    try to check if sorted?

--- home.py
+++ home.py
@@ -7,5 +7,8 @@ print(fibonacci(9))
 my_matrix = numpy.matrix('1 2; 3 4')
 print(my_matrix.getI() * my_matrix)
 my_list = [12, 5, 13, 8, 9, 65]
-print(bubble(my_list))
+if bubble(my_list) == [5, 8, 9, 12, 13, 65]:
+    print("success")
+else:
+    print("failure")
 print("bye")
