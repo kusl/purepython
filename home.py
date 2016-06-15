badac69dcb9496329b0482cda555783385c2c9d8
kushal <kushaldeveloper@gmail.com>
2016-06-15 09:36:04 -0400

    change data to sort

--- home.py
+++ home.py
@@ -6,7 +6,7 @@ from sort import bubble
 print(fibonacci(9))
 my_matrix = numpy.matrix('1 2; 3 4')
 print(my_matrix.getI() * my_matrix)
-my_list = [12, 5, 13, 8, 9, 65]
+my_list = [12, 5, 13, 8, 99, 65, 1]
 my_list = bubble(my_list)
 for index in range(1, 6):
     if my_list[index] < my_list[index - 1]:
