6460c7e5515c5e23d394871f75899d2668a88c57
kushal <kushaldeveloper@gmail.com>
2016-06-19 09:57:13 -0400

    sing 99 bottles of beer

--- home.py
+++ home.py
@@ -1,4 +1,6 @@
-import requests
-
-r = requests.get('https://api.github.com/events')
-print(r.content)
+for number_of_bottles in range(99, 0, -1):
+    print(str(number_of_bottles) + " bottles of beer on the wall")
+    print(str(number_of_bottles) + " bottles of beer")
+    print("take one down and pass it around")
+    print("you've got " + str(number_of_bottles - 1) + " of beer on the wall")
+    print()
