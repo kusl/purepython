f442d8efe4cb7f510ffa5fd060d2357430a433d1
kushal <kushaldeveloper@gmail.com>
2016-06-21 23:21:07 -0400

    use string.format

--- home.py
+++ home.py
@@ -1,6 +1,6 @@
 for number_of_bottles in range(99, 0, -1):
-    print(str(number_of_bottles) + " bottles of beer on the wall")
+    print("{0} bottles of beer on the wall".format(str(number_of_bottles)))
     print(str(number_of_bottles) + " bottles of beer")
     print("take one down and pass it around")
-    print("you've got " + str(number_of_bottles - 1) + " of beer on the wall")
+    print("you\'ve got {0} of beer on the wall".format(str(number_of_bottles - 1)))
     print()
